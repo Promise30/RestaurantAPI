1730ae9d3f7a1cded88652a3c7d75fe04e17f010
Promise30 <promiseadeagbo@gmail.com>
2023-08-23 16:16:54 +0100

    Added properties for Delivery class

---
+++ RestaurantAPI/Models/Delivery.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Delivery
+    {
+        public int DeliveryId { get; set; }
+        public bool OffersDelivery { get; set; }
+        public decimal DeliveryFee { get; set; }
+        public string DeliveryTime { get; set; }
+
+    }
+}
