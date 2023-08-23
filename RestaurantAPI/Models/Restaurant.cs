c60a5c66ebf4b7a5c2691a320f13efa82e32e0ec
Promise30 <promiseadeagbo@gmail.com>
2023-08-23 16:17:32 +0100

    Added properties for Restaurant entity

---
+++ RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+        [Required]
+        public string RestaurantName { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string? RestaurantImageUrl { get; set; }
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid phone number")]
+        public string? PhoneNumber { get; set; }
+        [DataType(DataType.Url)]
+        public string? WebsiteUrl { get; set; }
+        public string? StreetName { get; set; }
+        [Required]
+        public string Location { get; set; } = string.Empty;
+        [Required]
+        public string? OperationDetails { get; set; }
+        public string? ServiceType { get; set; }
+        public string? Cuisines { get; set; }
+        public bool IsOpen { get; set; } = true;
+        public double OverallRating { get; set; }
+        public int TotalReviews { get; set; }
+        public Delivery? Delivery { get; set; }
+        public ICollection<Review>? Reviews { get; set; }
+        public ICollection<MenuType>? MenuTypes { get; set; } = new List<MenuType>();
+        public int LocalGovernmentId { get; set; }
+        public virtual LocalGovernment LocalGovernment { get; set; }
+    }
+}
