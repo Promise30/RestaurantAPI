eb73f84bc9037346913f39930f67923386a33eba
Promise30 <promiseadeagbo@gmail.com>
2023-08-23 16:19:08 +0100

    Updated the entity mapping for creating menutypes and menuitems

---
+++ RestaurantAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using RestaurantAPI.DTOs;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<State, StateDTO>().ReverseMap();
+            CreateMap<Restaurant, DetailedRestaurantResponse>().ReverseMap();
+            //CreateMap<MenuType, MenuTypeDTO>().ForMember(dest => dest.MenuItems, opt => opt.MapFrom(src => src.MenuItems));
+            CreateMap<MenuItem, MenuItemDTO>().ReverseMap();
+            CreateMap<LocalGovernment, LocalGovtDTO>().ReverseMap();
+            CreateMap<Review, ReviewDTO>().ReverseMap();
+            CreateMap<Delivery, DeliveryDTO>().ReverseMap();
+            CreateMap<CreateMenuType, MenuTypeDTO>().ReverseMap();
+            CreateMap<MenuType, MenuTypeDTO>().ReverseMap();
+            CreateMap<CreateMenuItem, MenuItem>().ReverseMap();
+
+
+        }
+    }
+}
