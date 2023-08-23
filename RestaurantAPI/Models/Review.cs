fe41fba703536fec3b34cf131a4deaf3007d12b4
Promise30 <promiseadeagbo@gmail.com>
2023-08-23 16:17:14 +0100

    Added properties for Review entity

---
+++ RestaurantAPI/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string? ReviewerName { get; set; }
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        public int UpVoteCount { get; set; }
+        public int DownVoteCount { get; set; }
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 - 5")]
+        public int Rating { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public virtual ICollection<Vote>? Votes { get; set; }
+    }
+}
