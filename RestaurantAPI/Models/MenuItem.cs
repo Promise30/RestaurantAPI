d6d13fef8b92ba9790c8e565492fd9fc8290f48f
Promise30 <promiseadeagbo@gmail.com>
2023-08-25 18:39:35 +0100

    Added menuitem properties

---
+++ RestaurantAPI/Models/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int MenuItemId { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string MenuItemImageURL { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be more than 100 characters.")]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public int MenuTypeId { get; set; }
+        public virtual MenuType MenuType { get; set; }
+    }
+}
