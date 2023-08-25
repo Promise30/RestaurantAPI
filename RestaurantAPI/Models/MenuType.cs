d7de5909880e7828809fd8d951bd68706551022b
Promise30 <promiseadeagbo@gmail.com>
2023-08-25 18:39:22 +0100

    Added menutype properties

---
+++ RestaurantAPI/Models/MenuType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class MenuType
+    {
+        [Key]
+        public int MenuTypeId { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot be more than 50 characters")]
+        public string Name { get; set; }
+        public int RestaurantId { get; set; }
+        public virtual ICollection<MenuItem>? MenuItems { get; set; } = new List<MenuItem>();
+        public virtual Restaurant Restaurant { get; set; }
+
+    }
+}
