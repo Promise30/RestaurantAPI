e5b107f228d666f7db64c580704cee8e6e636195
Promise30 <promiseadeagbo@gmail.com>
2023-08-26 02:59:06 +0100

    Set default values for some properties

--- RestaurantAPI/Models/MenuType.cs
+++ RestaurantAPI/Models/MenuType.cs
@@ -8,9 +8,9 @@ namespace RestaurantAPI.Models
         public int MenuTypeId { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "Name cannot be more than 50 characters")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int RestaurantId { get; set; }
-        public virtual ICollection<MenuItem>? MenuItems { get; set; } = new List<MenuItem>();
+        public virtual ICollection<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
         public virtual Restaurant Restaurant { get; set; }
 
     }
