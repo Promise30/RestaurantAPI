810cba8723fb3120c322106f0fd777baa8e533fc
Promise30 <promiseadeagbo@gmail.com>
2023-08-26 02:59:34 +0100

    Defined properties for a User review

---
+++ RestaurantAPI/Models/ReviewSubmission.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class ReviewSubmission
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int Rating { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
