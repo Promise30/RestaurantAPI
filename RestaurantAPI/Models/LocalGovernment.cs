f9c7a0e22ab4b73fdfb6d531bf4392b5ee7be0e0
Promise30 <promiseadeagbo@gmail.com>
2023-08-27 03:01:14 +0100

    Defined initial properties for LocalGoverment model

---
+++ RestaurantAPI/Models/LocalGovernment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class LocalGovernment
+    {
+        [Key]
+        public int LocalGovernmentId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public int StateId { get; set; }
+
+        public virtual State State { get; set; }
+        public List<Restaurant> Restaurants { get; } = new();
+
+    }
+}
