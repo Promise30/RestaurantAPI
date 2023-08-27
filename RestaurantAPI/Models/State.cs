cbcb0039ab7276929b5bcb2ff7cbfcd131c6b36f
Promise30 <promiseadeagbo@gmail.com>
2023-08-27 03:00:46 +0100

    defined initial properties for State model

---
+++ RestaurantAPI/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class State
+    {
+        [Key]
+        public int StateId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<LocalGovernment> LocalGovernments { get; set; }
+
+    }
+}
