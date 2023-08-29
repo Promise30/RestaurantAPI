0b6a16c490756618f8586089d6355513bc3dd3bd
Promise30 <promiseadeagbo@gmail.com>
2023-08-29 05:36:14 +0100

    Initial properties for voting a review

---
+++ RestaurantAPI/Models/Vote.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Vote
+    {
+        public int VoteId { get; set; }
+        public bool IsUpVote { get; set; }
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
+    }
+}
